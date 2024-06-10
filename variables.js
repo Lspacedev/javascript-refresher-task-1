0265ff89727a5ae5a177ca0f725a1247f3bfdcc0
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-06-10 15:32:52 +0200

    added variables.js

---
+++ variables.js
@@ -0,0 +1,29 @@
+//Exercise 1: Variable Declaration
+
+let name = "Tshepo Mpogeng";
+let age = 23;
+let isStudent = false;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+//Exercise 2: Variable Reassignment
+let favoriteColour = "Black";
+console.log(favoriteColour);
+
+favoriteColour = "Blue";
+console.log(favoriteColour);
+
+//Exercise 3: Arithmetic Operations
+let num1 = 3;
+let num2 = 9;
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+let quotient = num2 / num1;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
